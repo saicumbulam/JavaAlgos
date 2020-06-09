5d9cd9ee668167e50e792adccba1de2ee28c959a
sadithya <sadithya@comscore.com>
2020-06-08 23:21:49 -0400

    Changed the position of old algorithmns and included a new leetcode ones

---
+++ JavaAlgorithm/src/Leetcode/Easy/LeafBinaryTree.java
@@ -0,0 +1,34 @@
+package Leetcode.Easy;
+
+import Leetcode.DataStructures.BST.BinarySearchTree;
+import Leetcode.DataStructures.BST.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeafBinaryTree {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        TreeNode root = bst.CreateBinaryTree();
+        List<Integer> result = new ArrayList<>();
+        getleaves(root, result);
+        bst.printTree2D(root, 5);
+        System.out.println(result);
+    }
+
+    private static void getleaves(TreeNode root, List<Integer> result) {
+        if (root == null)
+        {
+            return;
+        }
+        if (root.getLeft() == null && root.getRight() == null)
+        {
+            result.add(root.getData());
+        }
+        else
+        {
+            getleaves(root.getLeft(), result);
+            getleaves(root.getRight(), result);
+        }
+    }
+}
